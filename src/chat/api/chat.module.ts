fbca25a1b7a95c01986ccf3efab424dad68868b8
kasp608b <54952896+kasp608b@users.noreply.github.com>
2021-03-12 12:28:24 +0100

    Revert "Database stuff"
    This reverts commit 13e54ce74fa9a91c45c4885dffce5085ff0118aa.

--- src/chat/api/chat.module.ts
+++ src/chat/api/chat.module.ts
@@ -2,10 +2,8 @@ import { Module } from '@nestjs/common';
 import { ChatGateway } from './gateways/chat.gateway';
 import { ChatService } from '../core/services/chat.service';
 import { IChatServiceProvider } from '../core/primary-ports/chat.service.interface';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import client from '../infrastructure/data-source/client.entity';
+
 @Module({
-  imports: [TypeOrmModule.forFeature([client])],
   providers: [
     ChatGateway,
     {
